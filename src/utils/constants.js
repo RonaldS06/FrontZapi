d8c8e3cb53b5dc873b043f551d98667a13637b8b
Ronald Santamaria <pizarroronald06@gmail.com>
2024-07-31 16:50:11 -0300

    Firste Commit

---
+++ src/utils/constants.js
@@ -0,0 +1,6 @@
+// export const BASE_URL = "https://nucba-zapi-dun.vercel.app:8080";
+export const BASE_URL = "https://nodejs-prisma-production.up.railway.app";
+export const ADMIN = '50yun4dm1n';
+export const USER = '50yunu53r';
+
+/* eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNzIyNDUyNTE5LCJleHAiOjE3MjI0NTYxMTl9 Slxf82y2iADEWatoJSLVuZo_EAVkhMFQhR5fJtd628w */
